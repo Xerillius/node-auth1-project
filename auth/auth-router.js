95d790afa15b0dd7fbb9422c267122233efc23e6
Chris <caquinn.86@gmail.com>
2020-02-19 21:07:01 -0800

    finished part 2

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -5,7 +5,6 @@ const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
     let user = req.body;
-
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
 
@@ -25,13 +24,14 @@ router.post('/login', (req, res) => {
         .first()
         .then(user => {
             if(user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
                 res.status(200).json({message: `Welcome ${user.username}`});
             } else {
                 res.status(401).json({message: "Invalid Credentials"});
             }
         })
         .catch(err => {
-            res.status(500).json(error);
+            res.status(500).json(err);
         });
 });
 
